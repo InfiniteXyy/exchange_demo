f7d634d8da4f4a2e77607a85a75a1a29147f8cb5
InfiniteX <727160222@qq.com>
2018-05-12 16:18:24 +0800

    update post function

--- src/main/java/demo/controller/DemoController.java
+++ src/main/java/demo/controller/DemoController.java
@@ -15,7 +15,6 @@ public class DemoController {
             new Shop(2, "水币", "水的力量"),
             new Shop(3, "风币", "风的力量"),
             new Shop(4, "土币", "土的力量")
-
     );
 
     @RequestMapping("")
@@ -23,13 +22,26 @@ public class DemoController {
         return "demo.html";
     }
 
+    // unused
     // e.g. GET http://localhost:8080/shop/1
     @GetMapping("shop/{id}")
-    public @ResponseBody Shop getShop(@PathVariable("id") int id) {
+    public @ResponseBody
+    Shop getShop(@PathVariable("id") int id) {
         return shops.get(id - 1);
     }
+
     @GetMapping("shop/all")
-    public @ResponseBody Iterable<Shop> getAll() {
+    public @ResponseBody
+    Iterable<Shop> getAll() {
         return shops;
+    }
+
+    // subscribe a shop with param {name:'', shop:''}
+    // e.g. POST http://localhost:8080/subscribe?name=xyy&shop=1
+    @PostMapping("subscribe")
+    public @ResponseBody
+    String subscribe(@RequestParam String name, @RequestParam String shop) {
+        System.out.println(name + shop);
+        return "success";
     }
 }
